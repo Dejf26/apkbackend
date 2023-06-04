e1639ef996c56c505adc2b326b481e4227ff802c
dawid.migdal <dawid.migdal@PK6-10>
2023-06-04 12:55:03 +0200

    Dodaj pliki projektów.

---
+++ apkbackend/Pages/Models/Project.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace apkbackend.Pages.Models
+{
+    public class Project
+    {
+
+
+        public int Id { get; set; }
+        public string? Nazwa { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime Data_zgloszenia { get; set; }
+        public string status { get; set; }
+
+    }
+}
---
+++ apkbackend/Pages/Projects/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using apkbackend.Data;
+using apkbackend.Pages.Models;
+
+namespace apkbackend.Pages.Projects
+{
+    public class IndexModel : PageModel
+    {
+        private readonly apkbackend.Data.apkbackendContext _context;
+
+        public IndexModel(apkbackend.Data.apkbackendContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Project> Project { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Project != null)
+            {
+                Project = await _context.Project.ToListAsync();
+            }
+        }
+    }
+}
